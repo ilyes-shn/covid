e9cc3df23aaa0c7c329ebe549355d61585c360f0
ilyes-shn <infinityilyes@gmail.com>
2020-10-10 12:37:25 +0100

    cases

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,19 +5,19 @@ import Axios from 'axios'
 const Home = () => {
   
 const [place,setPlace] = useState('Algeria');
-const [data,setData] = useState({ ActiveCases: null, Death: null, Resolved: null})
+const [data,setData] = useState({ ActiveCases: null, Death: null, Resolved: null, Critical: null, Cases: null})
 
 function handleChange(e) {
 setPlace(e.target.value);
  if (e.target.value === '') { 
-   setData({ActiveCases: null, Death: null, Resolved: null}) }
+   setData({ActiveCases: null, Death: null, Resolved: null, Critical: null, Cases: null}) }
 }
 
 const search = () => {
 const result = Axios.get(`https://coronavirus-19-api.herokuapp.com/countries/${place}`)
   .then((data) => {
   setData({
-    ActiveCases: data.data.todayCases, Death: data.data.todayDeaths, Resolved: data.data.recovered
+    ActiveCases: data.data.todayCases, Death: data.data.todayDeaths, Resolved: data.data.recovered, Critical: data.data.critical, Cases: data.data.cases
   })
 })
 }
@@ -34,10 +34,12 @@ return (
     <div className="data">
     <h2>{data.ActiveCases} : حالة جديدة</h2>
     <h2>{data.Death} : الوفيات</h2>
+    <h2>{data.Critical} : الإنعاش</h2>
+    <h2>{data.Cases} : إجمالي الإصابات</h2>
     <h2>{data.Resolved} : حالات الشفاء</h2> 
     </div>
     </>)}
-
+  <footer><code>By Elyes</code></footer>
   </div>
   )
 };
